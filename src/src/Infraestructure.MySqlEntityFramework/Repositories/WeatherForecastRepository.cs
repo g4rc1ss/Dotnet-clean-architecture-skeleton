7e06a61e6137ec22d272588fdf6486dade751c5d
Asier <asiergarciab@outlook.com>
2022-05-08 00:35:30 +0200

    Update Action.yml (#2)
    Crear Github actions con docker-compose para la ejecucion de la base de datos

--- src/src/Infraestructure.MySqlEntityFramework/Repositories/WeatherForecastRepository.cs
+++ src/src/Infraestructure.MySqlEntityFramework/Repositories/WeatherForecastRepository.cs
@@ -1,9 +1,7 @@
 ﻿using Application.Interfaces.Data;
 using AutoMapper;
 using Domain.Database.ModelEntity;
-using Domain.Utilities.LoggingMediatr;
 using Infraestructure.MySqlEntityFramework.Contexts;
-using Infraestructure.MySqlEntityFramework.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,23 +9,20 @@ namespace Infraestructure.MySqlEntityFramework.Repositories
 {
     internal class WeatherForecastRepository : IWeatherForecastRepository
     {
-        private readonly CleanArchitectureSkeletonContext cleanArchitectureContext;
+        private readonly CleanArchitectureSkeletonContext _cleanArchitectureContext;
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
         public WeatherForecastRepository(IMapper mapper, CleanArchitectureSkeletonContext cleanArchitectureContext, IMediator mediator)
         {
             _mapper = mapper;
-            this.cleanArchitectureContext = cleanArchitectureContext;
+            _cleanArchitectureContext = cleanArchitectureContext;
             _mediator = mediator;
         }
 
-        private static readonly string[] Summaries = new[]
-        { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-
         public async Task<IEnumerable<WeatherForecastModelEntity>> GetWeatherForecastAsync()
-        {            
-            var consultaBaseDeDatos = await cleanArchitectureContext.WeatherForecasts.ToListAsync();
+        {
+            var consultaBaseDeDatos = await _cleanArchitectureContext.WeatherForecasts.ToListAsync();
             return _mapper.Map<IEnumerable<WeatherForecastModelEntity>>(consultaBaseDeDatos);
         }
     }

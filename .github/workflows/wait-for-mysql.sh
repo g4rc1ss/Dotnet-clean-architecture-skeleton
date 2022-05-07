e418f96af8404b3da2464037e321de9eb2a4ab9e
g4rc1ss <asiergarciab@outlook.com>
2022-05-07 17:25:03 +0200

    update

--- .github/workflows/wait-for-mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CONTAINER="mysql-principalDatabase"
-USERNAME="root"
-PASSWORD="123456"
-while ! docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
-    sleep 1
-done

d068af9193fe6cd972c854cb1917500bed22ebad
g4rc1ss <asiergarciab@outlook.com>
2022-05-08 18:54:48 +0200

    crear wpf

---
+++ src/Wpf/App.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Api.Extensions;
+using Wpf.Extensions;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Hellang.Middleware.ProblemDetails;
+using System;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            RunWebApiAsync();
+
+            RunWpfApplication();
+
+        }
+
+        private void RunWpfApplication()
+        {
+            var builder = Host.CreateDefaultBuilder();
+            builder.ConfigureServices(services =>
+            {
+                services.AddHttpClient("clienteLocalhost", (serviceProvider, httpClient) =>
+                {
+                    httpClient.BaseAddress = new Uri("http://localhost:5000/");
+                });
+
+                services.AddTransient<MainWindow>();
+            });
+
+            var app = builder.Build();
+            var window = app.Services.GetRequiredService<MainWindow>();
+            window.Show();
+        }
+
+        private async Task RunWebApiAsync()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            builder.Host.AddLoggerConfiguration();
+
+            builder.Services.AddCors(o => o.AddPolicy("localhostPolicyAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            builder.Services.InicializarConfiguracionApp(builder.Configuration);
+            builder.Services.AddProblemDetails();
+
+            builder.Services.AddControllers();
+            
+            var app = builder.Build();
+            app.Urls.Add("http://localhost:5000");
+            app.Urls.Add("http://localhost:5001");
+
+            app.UseProblemDetails();
+            app.UseCors("localhostPolicyAll");
+            
+            await app.RunAsync();
+        }
+    }
+}
---
+++ src/Wpf/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Wpf.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+
+        internal static IHostBuilder AddLoggerConfiguration(this IHostBuilder hostBuilder)
+        {
+            hostBuilder.UseSerilog((hostBuilderContext, loggerConfig) =>
+            {
+                loggerConfig.WriteTo.Async(config =>
+                {
+                    config.File("./logs", LogEventLevel.Debug);
+
+                    if (hostBuilderContext.HostingEnvironment.IsDevelopment())
+                    {
+                        config.Console(restrictedToMinimumLevel: LogEventLevel.Debug);
+                    }
+                });
+            });
+            return hostBuilder;
+        }
+    }
+}
